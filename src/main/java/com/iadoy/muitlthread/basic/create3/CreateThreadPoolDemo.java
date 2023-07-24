5561190449682e409b50e4f73354fef43d956f17
xcw <iadoy@outlook.com>
2023-07-24 13:24:32 +0800

    <feat>: 补充submit()方法获取返回值和异常的一些测试用例

--- src/main/java/com/iadoy/muitlthread/basic/create3/CreateThreadPoolDemo.java
+++ src/main/java/com/iadoy/muitlthread/basic/create3/CreateThreadPoolDemo.java
@@ -1,13 +1,12 @@
 package com.iadoy.muitlthread.basic.create3;
 
+import ch.qos.logback.core.testUtil.RandomUtil;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.Random;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -57,7 +56,7 @@ public class CreateThreadPoolDemo {
      * Executors创建可调度线程池
      */
     @Test
-    public void test() throws InterruptedException {
+    public void testScheduledExecutor() throws InterruptedException {
         ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
         //五轮循环，每轮执行两个任务
         for (int i = 0; i < 2; i++){
@@ -66,5 +65,57 @@ public class CreateThreadPoolDemo {
         }
         Thread.sleep(100000);
         service.shutdown();
+    }
+
+    /**
+     * 使用submit()向线程池提交任务，并获取Future对象
+     */
+    @Test
+    public void testSubmit() throws InterruptedException {
+        ScheduledExecutorService pool = Executors.newScheduledThreadPool(2);
+        Future<Integer> future = pool.submit(() -> {
+            Random r = new Random(System.currentTimeMillis());
+            return r.nextInt(1000);
+        });
+
+        try {
+            Integer integer = future.get();
+            log.info("异步执行结果：{}", integer);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            log.error("异步调用中断");
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            log.error("异步调用发生异常");
+        }
+        Thread.sleep(10 * 1000);
+        pool.shutdown();
+    }
+
+    /**
+     * 演示submit()过程中发生的异常如何捕获
+     */
+    @Test
+    public void testSubmitException() throws InterruptedException {
+        ExecutorService pool = Executors.newSingleThreadExecutor();
+        //提交一个新的异步任务，该异步任务会抛出异常
+        Future<Object> future = pool.submit(() -> {
+            throw new RuntimeException("异步任务出错了");
+        });
+
+        try {
+            //同样用get()方法获取，如果获取到的是执行过程中的异常，会由下面的catch捕获
+            Object o = future.get();
+            log.info("任务完成，结果为{}", o);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            log.error("异步调用中断");
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            log.error("异步调用发生异常，异常为{}", e.getMessage());
+        }
+
+        Thread.sleep(10 * 1000);
+        pool.shutdown();
     }
 }
