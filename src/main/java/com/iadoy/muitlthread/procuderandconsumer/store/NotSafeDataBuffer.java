9e8e69de44db78df5cb612bee30cd11c6a6368cb
xcw <iadoy@outlook.com>
2023-07-25 21:57:39 +0800

    <feat>: 不安全的数据缓冲区

---
+++ src/main/java/com/iadoy/muitlthread/procuderandconsumer/store/NotSafeDataBuffer.java
@@ -0,0 +1,49 @@
+package com.iadoy.muitlthread.procuderandconsumer.store;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 不安全的数据缓冲区
+ * @param <T>
+ */
+@Slf4j
+public class NotSafeDataBuffer<T> {
+    public static final int MAX_AMOUNT = 10;
+    private List<T> dataList = new LinkedList<>();
+    //保存数量
+    private AtomicInteger amount = new AtomicInteger(0);
+
+    public void add(T element) throws Exception {
+        if (amount.get() >= MAX_AMOUNT){
+            log.info("队列已经满了");
+            return;
+        }
+        dataList.add(element);
+        log.info("加入元素{}", element);
+        // 更新数据缓冲区数量
+        amount.incrementAndGet();
+
+        if (amount.get() != dataList.size()){
+            throw new Exception("数据不一致");
+        }
+    }
+
+    public T fetch() throws Exception {
+        if (amount.get() <= 0){
+            log.info("队列已经空了");
+            return null;
+        }
+        T element = dataList.remove(0);
+        log.info("取出数据{}", element);
+        amount.decrementAndGet();
+
+        if (amount.get() != dataList.size()){
+            throw new Exception("数据不一致");
+        }
+        return element;
+    }
+}
